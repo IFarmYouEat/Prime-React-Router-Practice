61d969e3379b84ea3d3be46e968422f98c74faf7
Brock Nelson <Bmnelson87@gmail.com>
2023-11-07 13:28:44 -0600

    adjusted About page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,7 @@ function About() {
     return(
         <div>
             <h3>Welcome to our Bootcamp</h3>
-            <p>At Prime Digital Academy we have learned many things.</p>
+            <p>At Prime Digital Academy, we are a dynamic group of programming students.</p> 
         </div>
     )
 }
